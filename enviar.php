93dd44a5facb301e8b5c6f9eaf10aec57f45513e
Slinkter <luis.j.cueva@gmail.com>
2019-04-12 10:04:41 -0500

    2019 04 12 : php email

---
+++ enviar.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$form_name = $_POST["form_name"];
+$form_email = $_POST["form_email"];
+$form_tel = $_POST["form_tel"];
+$form_msj = $_POST["form_msj"];
+
+
+
+$para      = 'juancarlos@vidrieriachaloreyes.com';
+$titulo    = 'Correo de Pagina Web';
+$mensaje   = 'Nombre : '.$form_name  ."\n";
+$cabeceras = 'From: webmaster@vidrieriachaloreyes.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($para, $titulo, $mensaje, $cabeceras);
+header("Location:gracias.html");
+
+
+?>
